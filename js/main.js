0b48d5b85f5480ffd4937c6d2572b43f23404502
Brittanie Pham <brittaniepham@gmail.com>
2018-05-15 15:33:11 -0600

    create compare functions(not implemented)

--- js/main.js
+++ js/main.js
@@ -4,44 +4,97 @@ $(document).ready( function() {
   //listeners
   //code that runs
 
-  //VARIABLES
+  //VARIABLES -------------------------------------------------
   var userChoice
   var options = ['rock', 'paper', 'scissors']
   var compChoice
 
-  //FUNCTIONS
-  function updateUserScore() {
-  }
-
-  function updateCompScore() {
-  }
+  //FUNCTIONS -------------------------------------------------
 
   function compChooses() {
     compChoice = options[Math.floor(Math.random() * options.length)]
   }
 
-  function printScore() {
+  function printChoices() {
     $('#userClicked').text('You chose: ' + userChoice)
     $('#compClicked').text('The computer chose: ' + compChoice)
   }
 
-  //LISTENERS
+  function compare(compChoice) {
+    compareUserRock(compChoice)
+    compareUserPaper(compChoice)
+    compareUserScissors(compChoice)
+  }
+
+  function compareUserRock(compChoice) {
+    userChoice = 'rock'
+    outcome = $('#outcome')
+    switch(compChoice) {
+      case 'rock':
+        outcome.text("IT'S A TIE!")
+        break
+      case 'paper':
+        outcome.text("PAPER BEATS ROCK... YOU LOSE")
+        break
+      case 'scissors':
+        outcome.text("ROCK BEATS SCISSORS! YOU WIN!")
+        break
+    }
+  }
+
+  function compareUserPaper(compChoice) {
+    userChoice = 'paper'
+    outcome = $('#outcome')
+    switch(compChoice) {
+      case 'rock':
+        outcome.text("PAPER BEATS ROCK! YOU WIN!")
+        break
+      case 'paper':
+        outcome.text("IT'S A TIE!")
+        break
+      case 'scissors':
+        outcome.text("SCISSORS BEATS PAPER... YOU LOSE")
+        break
+    }
+  }
+
+  function compareUserScissors(compChoice) {
+    userChoice = 'scissors'
+    outcome = $('#outcome')
+    switch(compChoice) {
+      case 'rock':
+        outcome.text("ROCK BEATS SCISSORS... YOU LOSE")
+        break
+      case 'paper':
+        outcome.text("SCISSORS BEATS PAPER! YOU WIN!")
+        break
+      case 'scissors':
+        outcome.text("IT'S A TIE!")
+        break
+    }
+  }
+
+  // LISTENERS -------------------------------------------------
+
   $('#rock').on('click', function() {
     userChoice = 'rock'
     compChooses()
-    printScore()
+    printChoices()
+    compare()
   })
 
   $('#paper').on('click', function() {
     userChoice = 'paper'
     compChooses()
-    printScore()
+    printChoices()
+    compare()
   })
 
   $('#scissors').on('click', function() {
     userChoice = 'scissors'
     compChooses()
-    printScore()
+    printChoices()
+    compare()
   })
 
 })
